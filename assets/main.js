7c0fc96b435dfb6cd88f16e015ac28333bcaa424
Kexin996 <kz2031@nyu.edu>
2022-07-08 22:34:35 -0400

    finish slide bar

--- assets/main.js
+++ assets/main.js
@@ -4,3 +4,20 @@ let collapse = document.querySelectorAll("#header .collapse");
 toggle.addEventListener('click',function(){
     collapse.forEach(col => col.classList.toggle("collapse-toggle"));
 })
+
+// with masonry
+new Masonry("#posts .grid",{
+    ItemSelector:".grid-item",
+    gutter:20
+});
+
+// swiper library initialization
+new Swiper(".swiper-container",{
+    direction:"horizontal",
+    loop:true,
+    slidesPerView:5,
+    autoplay:
+    {
+        delay:3000
+    }
+})
