55a244b06518aba9e82fef9cbc665dbff0b906b3
Kexin996 <kz2031@nyu.edu>
2022-07-08 20:47:21 -0400

    debug successfully

--- assets/main.js
+++ assets/main.js
@@ -2,5 +2,5 @@ let toggle = document.querySelector("#header .toggle-button");
 let collapse = document.querySelectorAll("#header .collapse");
 
 toggle.addEventListener('click',function(){
-    collapse.forEach(col=>col.classList.toggle("collapse-toggle"));
+    collapse.forEach(col => col.classList.toggle("collapse-toggle"));
 })
